94ba675f82db5de4a8683b6c28c7e6c4ede5acb0
PaupietteMasta <frablin@gmail.com>
2020-07-29 09:01:27 +0200

    back-end config

--- vueauthclient/src/components/Dashboard.vue
+++ vueauthclient/src/components/Dashboard.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div>
     <h2>Dashboard</h2>
@@ -6,34 +5,34 @@
   </div>
 </template>
 <script>
-    import axios from "axios"    
-    import router from "../router"    
-    export default {    
-        name: "Login",    
-        data() {    
-            return {    
-                user: {    
-                    name:“Jesse”    
-                }    
-            }    
-        },    
-        methods: {    
-            getUserData: function() {    
-                let self = this    
-                axios.get("/api/user")    
-                    .then((response) => {    
-                        console.log(response)    
-                        self.$set(this, "user", response.data.user)    
-                    })    
-                    .catch((errors) => {    
-                        console.log(errors)    
-                        router.push("/")    
-                    })    
-            }    
-        },    
-        mounted() {    
-            this.getUserData()    
-        }    
+import axios from 'axios'
+import router from '../router'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        name: 'Jesse'
+      }
     }
+  },
+  methods: {
+    getUserData: function () {
+      let self = this
+      axios
+        .get('/api/user')
+        .then(response => {
+          console.log(response)
+          self.$set(this, 'user', response.data.user)
+        })
+        .catch(errors => {
+          console.log(errors)
+          router.push('/')
+        })
+    }
+  },
+  mounted () {
+    this.getUserData()
+  }
+}
 </script>
-
--- vueauthclient/src/components/Login.vue
+++ vueauthclient/src/components/Login.vue
@@ -12,32 +12,32 @@
 </template>
 
 <script>
-import router from "../router";
-import axios from "axios";
+import router from '../router'
+import axios from 'axios'
 export default {
-  name: "Login",
+  name: 'Login',
   methods: {
     login: e => {
-      e.preventDefault();
-      let email = "user@email.com";
-      let password = "password";
+      e.preventDefault()
+      let email = 'user@email.com'
+      let password = 'password'
       let login = () => {
         let data = {
           email: email,
           password: password
-        };
+        }
         axios
-          .post("/api/login", data)
+          .post('/api/login', data)
           .then(response => {
-            console.log("Logged in");
-            router.push("/dashboard");
+            console.log('Logged in')
+            router.push('/dashboard')
           })
           .catch(errors => {
-            console.log("Cannot log in");
-          });
-      };
-      login();
+            console.log('Cannot log in')
+          })
+      }
+      login()
     }
   }
-};
+}
 </script>
